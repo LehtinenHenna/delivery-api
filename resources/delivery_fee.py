06c0ee739469c09b4d982cfcc4d5d8ef003944e8
Henna Lehtinen <henna@iontau.com>
2024-01-19 15:53:24 +0200

    Make a working skeleton of DeliveryFeeResource

--- resources/delivery_fee.py
+++ resources/delivery_fee.py
@@ -1,7 +1,8 @@
 from flask import request
 from flask_restful import Resource
+from http import HTTPStatus
 
-class DeliveryFeeResource:
+class DeliveryFeeResource(Resource):
 
     def post(self):
         '''
@@ -9,4 +10,6 @@ class DeliveryFeeResource:
         POST -> /deliveryfee
         '''
         json_data = request.get_json()
+        print('json_data', json_data)
         # Calculate delivery fee based on parameters in json_data, return as {"delivery_fee": 710} with status 200 OK
+        return {'delivery_fee': 0}, HTTPStatus.OK
