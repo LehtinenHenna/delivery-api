80e071a04eab9c7f32c52b6cb3da959e07c1b043
Henna Lehtinen <henna@iontau.com>
2024-01-19 17:08:23 +0200

    validate request data

--- resources/delivery_fee.py
+++ resources/delivery_fee.py
@@ -9,7 +9,21 @@ class DeliveryFeeResource(Resource):
         Calculate delivery fee based on parameters in the request.
         POST -> /deliveryfee
         '''
-        json_data = request.get_json()
-        print('json_data', json_data)
+        request_dict = request.get_json()
+        print('type(request_dict)', type(request_dict))
+        
+        # Validate request data
+        required_keys = (
+            "cart_value", 
+            "delivery_distance", 
+            "number_of_items", 
+            "time"
+        )
+        for key in required_keys:
+            if key not in request_dict:
+                return {"message": "One or more required keys missing from the request. Required keys: {}".format(required_keys)}
+        
         # Calculate delivery fee based on parameters in json_data, return as {"delivery_fee": 710} with status 200 OK
-        return {'delivery_fee': 0}, HTTPStatus.OK
+        delivery_fee = 0
+            
+        return {'delivery_fee': delivery_fee}, HTTPStatus.OK
