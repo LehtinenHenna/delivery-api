2151449dafe920c84dbc8c9c73926e8453e49613
Henna Lehtinen <henna@iontau.com>
2024-01-19 15:52:09 +0200

    Make a runnable flask app

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api
+from resources.delivery_fee import DeliveryFeeResource
 
 class DeliveryApi:
 
     def __init__(self):
         self.app = Flask(__name__)
+        self.api = Api(self.app)
+        self.register_resources()
+
+    def register_resources(self):
+        self.api.add_resource(DeliveryFeeResource, '/delivery-fee')
 
     def run(self):
-        pass
+        self.app.run(debug=True)
     
 if __name__ == '__main__':
     delivery_api = DeliveryApi()
