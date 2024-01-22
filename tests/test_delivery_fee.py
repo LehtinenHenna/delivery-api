19b1b3ae789a3f2cf128c5a890759d00c78508b5
Henna Lehtinen <henna@iontau.com>
2024-01-22 14:04:17 +0200

    add unit test for delivery fee post endpoint

--- tests/test_delivery_fee.py
+++ tests/test_delivery_fee.py
@@ -1,4 +1,14 @@
+import pytest
 from main import DeliveryApi
+from test_parameters import delivery_fee_post_parameters
 
-def test_api():
-    response = DeliveryApi.app
+
+@pytest.mark.parametrize(
+    "request_json, expected_response, expected_http_status", delivery_fee_post_parameters
+)
+def test_delivery_fee_post(request_json, expected_response, expected_http_status):
+    delivery_api = DeliveryApi()
+    with delivery_api.app.test_client() as client:
+        response = client.post('/delivery-fee', json=request_json)
+        assert response.status_code == expected_http_status
+        assert response.data == expected_response
