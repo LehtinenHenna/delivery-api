ab0c464ca0223ef965e2416fafe9f8a7d32b433e
Henna Lehtinen <henna@iontau.com>
2024-01-25 17:44:03 +0200

    add docstring

--- tests/test_delivery_fee.py
+++ tests/test_delivery_fee.py
@@ -1,13 +1,14 @@
 import pytest
 from json import loads
 from main import DeliveryApi
-from test_parameters import delivery_fee_post_parameters
+from parameters import delivery_fee_post_parameters
 
 
 @pytest.mark.parametrize(
     "request_json, expected_response, expected_http_status", delivery_fee_post_parameters
 )
-def test_delivery_fee_post(request_json, expected_response, expected_http_status):
+def test_delivery_fee_post(request_json: dict, expected_response: dict, expected_http_status: int):
+    '''Tests the POST endpoint at URL /delivery-fee with multiple test cases.'''
     delivery_api = DeliveryApi()
     with delivery_api.app.test_client() as client:
         response = client.post('/delivery-fee', json=request_json)
