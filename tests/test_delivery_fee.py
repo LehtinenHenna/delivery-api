6d8429f93c8158ac710b3b6aa64f375fc4dbf0d6
Henna Lehtinen <henna@iontau.com>
2024-01-25 09:55:05 +0200

    fix datatype of response data

--- tests/test_delivery_fee.py
+++ tests/test_delivery_fee.py
@@ -1,4 +1,5 @@
 import pytest
+from json import loads
 from main import DeliveryApi
 from test_parameters import delivery_fee_post_parameters
 
@@ -11,4 +12,4 @@ def test_delivery_fee_post(request_json, expected_response, expected_http_status
     with delivery_api.app.test_client() as client:
         response = client.post('/delivery-fee', json=request_json)
         assert response.status_code == expected_http_status
-        assert response.data == expected_response
+        assert loads(response.data) == expected_response
