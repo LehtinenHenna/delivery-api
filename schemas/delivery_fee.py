91757e4ea344113fe809b48ec06da618fbd646f7
Henna Lehtinen <henna@iontau.com>
2024-01-26 14:43:21 +0200

    add docstring

--- schemas/delivery_fee.py
+++ schemas/delivery_fee.py
@@ -1,6 +1,23 @@
 from marshmallow import Schema, fields
 
 class DeliveryFeeSchema(Schema):
+    '''
+    Validates JSON data with marshmallow.
+    
+    ...
+
+    Attributes
+    ----------
+    cart_value: fields.Integer
+        Validates that the field is given and that it's of type int.
+    delivery_distance: fields.Integer
+        Validates that the field is given and that it's of type int.
+    number_of_items: fields.Integer
+        Validates that the field is given and that it's of type int.
+    time: fields.String
+        Validates that the field is given and that it's of type str. 
+    '''
+
     cart_value = fields.Integer(required=True)
     delivery_distance = fields.Integer(required=True)
     number_of_items = fields.Integer(required=True)
