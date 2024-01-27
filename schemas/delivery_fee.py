d98d22444f829f200f50aa77c94b07709a6f5900
Henna Lehtinen <henna@iontau.com>
2024-01-27 20:43:19 +0200

    add validation for numbers to be positive and time to be UTC datetime object

--- schemas/delivery_fee.py
+++ schemas/delivery_fee.py
@@ -3,7 +3,8 @@
 This module contains the class DeliveryFeeSchema 
 for validating JSON data.
 '''
-from marshmallow import Schema, fields
+from datetime import timezone, datetime
+from marshmallow import Schema, fields, validate, validates, ValidationError
 
 class DeliveryFeeSchema(Schema):
     '''
@@ -14,16 +15,34 @@ class DeliveryFeeSchema(Schema):
     Attributes
     ----------
     cart_value: fields.Integer
-        Validates that the field is given and that it's of type int.
+        Validates that the field is given, and that it's of type int,
+        and that the number is greater than zero.
     delivery_distance: fields.Integer
-        Validates that the field is given and that it's of type int.
+        Validates that the field is given, and that it's of type int,
+        and that the number is greater than zero.
     number_of_items: fields.Integer
-        Validates that the field is given and that it's of type int.
-    time: fields.String
-        Validates that the field is given and that it's of type str. 
+        Validates that the field is given, and that it's of type int,
+        and that the number is greater than zero.
+    time: fields.AwareDateTime
+        Validates that the field is given, and that it's of type datetime.
+    
+    Methods
+    -------
+    validates_time(time_as_datetime: datetime)
+        Provides extra validation for time to make sure it's in UTC time zone.
     '''
 
-    cart_value = fields.Integer(required=True)
-    delivery_distance = fields.Integer(required=True)
-    number_of_items = fields.Integer(required=True)
-    time = fields.String(required=True)
+    cart_value = fields.Integer(required=True, validate=[
+        validate.Range(min=1, error="Value must be greater than 0.")])
+    delivery_distance = fields.Integer(required=True, validate=[
+        validate.Range(min=1, error="Value must be greater than 0.")])
+    number_of_items = fields.Integer(required=True, validate=[
+        validate.Range(min=1, error="Value must be greater than 0.")])
+    time = fields.AwareDateTime(required=True)
+
+    @validates("time")
+    def validates_time(self, time_as_datetime: datetime) -> datetime:
+        '''Validates that time is in UTZ timezone.'''
+        if time_as_datetime.tzinfo != timezone.utc:
+            raise ValidationError('Not a valid datetime.')
+        return time_as_datetime
